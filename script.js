9b120af02bcbecc0b0a80da0e3a67acad3307ba4
AzizManiyazov <dj.azik93@gmail.com>
2024-12-12 07:36:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let Name = prompt('Как Вас зовут?');
+alert("Добро пожаловать " + Name)
+
+let account = prompt('Номер счёта');
+alert("Баланс 10000$"); 
+
+let money = prompt('Сколько обналичать?');
+console.log(money);
+
+// let money2 = prompt('Все отлично');
+alert("Все отлично");
